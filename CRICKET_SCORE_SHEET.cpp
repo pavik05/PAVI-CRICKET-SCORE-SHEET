4992bcaed892b0338759fc7e361c5268538187dd
K.PAVITHRA <81957765+pavik05@users.noreply.github.com>
2021-07-15 00:37:47 -0700

    Update and rename README.md to CRICKET_SCORE_SHEET.cpp

---
+++ CRICKET_SCORE_SHEET.cpp
@@ -0,0 +1,8 @@
+//PAVI-CRICKET-SCORE-SHEET
+/*PROBLEM STATEMENT:-
+Write a C program to display the 
+Name of the Player(Batsman/Bowler),
+Their Batting Average,
+Runs,Overs and Economy Rate 
+using “File Concepts”.*/
+
